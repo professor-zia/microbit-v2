b0467efbc3a6a5e8bd2cc9c4d03275e7fe8e8cef
Professor Zia <fzia@citytech.cuny.edu>
2022-11-20 22:11:32 -0500

    update 2022_1120

---
+++ software/unit-test-code/code-template.ino
@@ -0,0 +1,46 @@
+/*** header block ***
+ * code file name:
+ * code description:
+ * arduino board (UNO/Nano/...etc): 
+ * sensors and devices needed:
+ * IDE version used to test code:
+ * programmer(s) name:
+ * date when code is created/modified:
+ * code version/revision:
+ ***/
+
+// compiler directives
+
+// include libraries
+
+// pin name definitions
+
+// class/object declarations
+
+// global variables
+
+
+void setup () {
+   // configure hardware peripherals
+   
+   // configure data communication
+   
+   // code that runs only once 
+   
+}
+
+void loop () {
+   // data input
+   
+   // data storage
+   
+   // data processing
+   
+   // data output
+   
+   // data communication
+   
+}
+
+// user defined functions
+
--- software/unit-test-code/ubit-test-digital-out/ubit-test-digital-out.ino
+++ software/unit-test-code/ubit-test-digital-out/ubit-test-digital-out.ino
@@ -1,23 +1,50 @@
+/*** header block ***
+ * code file name: ubit-test-digital-out.ino
+ * hardware board (UNO/Nano/microbit..etc): microbit v2.0
+ * sensors and devices needed: builtin 5x5 LED matrix
+ * code description: 
+        this code tests digital output hardware functionality
+        by blinking one (center) LED on the 5x5 LED matrix
+ * IDE version used to test code: Arduino IDE v2.0.2
+ * programmer(s) name: Instructor
+ * date when code is created/modified: 2022_1120
+ * code version/revision: v1.0
+ ***/
+
+// compiler directives (none)
+
+// include libraries (none)
+
+// pin name definitions
 // see variant.cpp for arduino pin numbers for microbit v2
-const int COL =   3; // col 3 control
-const int ROW =  23; // row 3 control
-const int LED = ROW; // row 3 led
+const int COL = 3;    // col 3 control
+const int ROW = 23;   // row 3 control
+const int LED = ROW;  // row 3 led
+
+// class/object declarations (none)
+
+// global variables (none)
 
 void setup() {
-  Serial.begin(9600);
-
-  Serial.println("microbit is ready!");
-
+  // configure hardware peripherals
   // ground LED cathode
   pinMode(COL, OUTPUT);
   digitalWrite(COL, LOW);
-
+  // set ROW pin as digital output pin
   pinMode(ROW, OUTPUT);
+
+  // configure data communication
+  Serial.begin(9600);
+  // code that runs only once
+  Serial.println("microbit is ready!");
 }
 
 void loop() {
+  // data comm operation
+  // print heart-beat message
   Serial.println("blink!");
 
+  // data output operations
   digitalWrite(LED, HIGH);
   delay(100);
   digitalWrite(LED, LOW);
